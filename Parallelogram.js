4dae5a7918cacd6b02422b61f51c83c398b55580
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-02-13 21:38:27 +0600

    work

---
+++ Parallelogram.js
@@ -0,0 +1,20 @@
+const mainName = document.getElementById('ParallelogramButton').addEventListener('click', () => {
+    const paralelogramBase = document.getElementById('parallelogramBase');
+    const base = paralelogramBase.value;
+    const paral = parseFloat(base);
+    console.log(paral);
+
+    const paralelogramHeigh =  document.getElementById('parallelogramHeight');
+    const height = paralelogramHeigh.value;
+    const paral2 = parseFloat(height);
+    console.log(paral2);
+
+    const result = paral * paral2;
+    console.log(result);
+
+    const areas = document.getElementById('ParallelogramlArea');
+    areas.innerHTML = result;
+
+    paralelogramBase.value = "";
+    paralelogramHeigh.value = "";
+});
